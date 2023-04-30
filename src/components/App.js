d608c503c27ce388db40c1dc95f98d6a4518478e
Neetu99 <120268912+Neetu99@users.noreply.github.com>
2023-04-30 13:32:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,13 @@ import SubmitButton from "./SubmitButton";
 
 const App = () =>{
 return (
-  <div id="main"><Heading/><InputQuery/><SubmitButton/></div>)}
+  <div id="main">
+  <Heading/>
+  <SubHeading/>
+  <InputQuery/>
+  <SubmitButton/>
+  </div>)
+  }
 export default App;
 
 
